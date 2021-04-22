16b4baba7bad2d9cf6859f938f90bcd2d68ff6ac
Everton <everton.ferreira@luizalabs.com>
2021-04-22 18:04:31 -0300

    navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h3>Navbar</h3>
-      </div>
+      /* https://www.iconfinder.com/icons/1243689/call_phone_icon Creative
+        Commons (Attribution 3.0 Unported);
+        https://www.iconfinder.com/Makoto_msk */
+      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="store-logo" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav alight-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <button>
+            <FontAwesomeIcon icon={faCartArrowDown} />
+          </button>
+        </Link>
+      </nav>
     );
   }
 }
