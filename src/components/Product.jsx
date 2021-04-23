14774c921c1d4c71cdf6f718cdb2d8eb1e035e35
Everton <everton.ferreira@luizalabs.com>
2021-04-23 18:10:17 -0300

    details

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { PropTypes } from "prop-types";
+import { ProductConsumer } from "../context";
 
 export default class Product extends Component {
   render() {
@@ -11,28 +12,32 @@ export default class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div
-            className="img-container p-5"
-            onClick={() => console.log("clicou")}
-          >
-            <Link to="/details">
-              <img src={img} alt="product" className="card-img-top" />
-            </Link>
-            <button
-              className="cart-btn"
-              disabled={inCart}
-              onClick={() => console.log("cart")}
-            >
-              {inCart ? (
-                <p className="text-capitalize mb-0" disabled>
-                  {" "}
-                  in cart
-                </p>
-              ) : (
-                <FontAwesomeIcon icon={faCartPlus} />
-              )}
-            </button>
-          </div>
+          <ProductConsumer>
+            {(value) => (
+              <div
+                className="img-container p-5"
+                onClick={() => value.handleDetail(id)}
+              >
+                <Link to="/details">
+                  <img src={img} alt="product" className="card-img-top" />
+                </Link>
+                <button
+                  className="cart-btn"
+                  disabled={inCart}
+                  onClick={() => value.addToCart(id)}
+                >
+                  {inCart ? (
+                    <p className="text-capitalize mb-0" disabled>
+                      {" "}
+                      in cart
+                    </p>
+                  ) : (
+                    <FontAwesomeIcon icon={faCartPlus} />
+                  )}
+                </button>
+              </div>
+            )}
+          </ProductConsumer>
           <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center mb-0">{title}</p>
             <h5 className="text-blue font-italic mb-0">
